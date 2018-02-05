bbcd97b1f008736a9cb62fb13fc09f4d3b3ef78b
YoonSuMin <gmfrsp@naver.com>
2018-02-06 01:59:27 +0900

    Prototype 0208
    Edit

---
+++ Prototype_Deepdown/Assets/02. Scripts/KDH/KDHCloseDoor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KDHCloseDoor : MonoBehaviour
+{
+   
+    public GameObject TileTrigger;
+ 
+
+    KDHCloseDoorTrigger tile;
+
+
+    public float rotate = 1.0f;
+    float rot = 0.0f;
+    float angle = 0.0f;
+
+    public bool isOpen;
+    bool isClose = false;
+
+    void Start()
+    {
+        tile = TileTrigger.GetComponent<KDHCloseDoorTrigger>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tile.GetisOpen() && !isClose)
+        {
+            isClose = true;
+            RotateCloseDoor();
+        }
+    }
+
+    void RotateCloseDoor()
+    {
+        //angle++;
+        //Vector3 rot = transform.eulerAngles;
+        //rot.y += rotate;
+        //transform.eulerAngles = rot;
+        //if (angle < 90)
+        //    Invoke("RotateCloseDoor", Time.deltaTime);
+        //Quaternion rot = transform.rotation;
+        //transform.rotation = Quaternion.Slerp(rot, Quaternion.Euler(rot.x, rot.y + (angle * 2), rot.z), 5.0f * Time.deltaTime);
+
+        iTween.RotateTo(transform.gameObject, new Vector3(0, angle, 0), 3.0f);
+
+        //StartCoroutine(KDHCloseDoorTrigger.instance.TelePortPlayer());
+    }
+
+
+}
---
+++ Prototype_Deepdown/Assets/02. Scripts/KDH/KDHOpenDoorTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KDHOpenDoorTrigger : MonoBehaviour {
+
+    bool isOpen;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Open");
+            isOpen = true;
+        }
+    }
+ 
+    public bool GetisOpen()
+    {
+        return isOpen;
+    }
+}
