5704bc1396d7fd7d9d9cdbb75a0d838e554cd2b9
YoonSuMin <gmfrsp@naver.com>
2018-02-06 02:11:41 +0900

    Revert "Revert "Prototype 0208""
    This reverts commit 533fbdfc083f4dca779bccb54634a7225c838554.

---
+++ Prototype_Deepdown/Assets/02. Scripts/YSM/YSM_CameraWork.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class YSM_CameraWork : MonoBehaviour {
+    public Text txt;
+    public VideoPlayer VideoPlayer;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            VideoPlayer.enabled = false;
+        }
+    }
+
+    private void Init()
+    {
+        txt.enabled = false;
+        //VideoPlayer = GameObject.Find("NearCameraVideo").GetComponent<VideoPlayer>();
+        VideoPlayer.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name.Contains("Stage"))
+        {
+            if (VideoPlayer.enabled == false)
+            {
+                txt.enabled = true;
+
+                if (Input.GetKeyDown(KeyCode.V))
+                {
+                    Debug.Log("video");
+                    txt.enabled = false;
+                    VideoPlayer.enabled = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name.Contains("Stage"))
+        {
+            txt.enabled = false;
+        }
+    }
+}
